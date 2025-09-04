fc6fe807f5cf2b642bcb15d9533e96dbc475d01f
Azalea-u <salmi.mouad137@gmail.com>
2025-09-04 10:59:30 +0100

    ⚪

--- fibonacci2/src/lib.rs
+++ fibonacci2/src/lib.rs
@@ -1,6 +1,9 @@
 pub fn fibonacci(n: u32) -> u32 {
-    if n <= 2 {
-        return 1;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    } 
 }
