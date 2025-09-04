1d463b43cb28f9f27b774181bf034e55455dfd76
Azalea-u <salmi.mouad137@gmail.com>
2025-09-04 10:13:46 +0100

    ⚖️

---
+++ scalar/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn sum(a: u8, b: u8) -> u8 {
+    let c = a+b;
+    return c;
+}
+
+pub fn diff(q: i16, z: i16) -> i16 {
+    let w = q-z;
+    return w;
+}
+
+pub fn pro(q: i8, z: i8) -> i8 {
+    let w = q*z;
+    return w;
+}
+
+pub fn quo(q: f32, z: f32) -> f32 {
+    let w = q/z;
+    return w;
+}
+
+pub fn rem(q: f32, z: f32) -> f32 {
+    let w = q%z;
+    return w;
+}
