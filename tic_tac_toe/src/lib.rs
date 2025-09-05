c5405fffb4b451363018e10d04b98c0c2feacf25
Azalea-u <salmi.mouad137@gmail.com>
2025-09-05 10:57:42 +0100

    ...

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
-    if has_won('O', &table) {
+    if has_won('O', table) {
         "player O won".to_string()
-    } else if has_won('X', &table) {
+    } else if has_won('X', table) {
         "player X won".to_string()
     } else {
         "tie".to_string()
@@ -12,9 +12,9 @@ fn has_won(player: char, table: [[char; 3]; 3]) -> bool {
     horizontal(player, table) || vertical(player, table) || diagonals(player, table)
 }
 
-pub fn horizontal(player: char, table: &[[char; 3]; 3]) -> bool {
-    for row in table {
-        if (0..3).all(|col| row[col] == player) {
+pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
+    for row in &table {
+        if row.iter().all(|&c| c == player) {
             return true;
         }
     }
@@ -31,11 +31,7 @@ pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
 }
 
 pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
-    let mut main_diagonal = true;
-    let mut anti_diagonal = true;
-    for i in 0..3 {
-        main_diagonal = main_diagonal && table[i][i] == player;
-        anti_diagonal = anti_diagonal && table[i][2 - i] == player;
-    }
+    let main_diagonal = (0..3).all(|i| table[i][i] == player);
+    let anti_diagonal = (0..3).all(|i| table[i][2 - i] == player);
     main_diagonal || anti_diagonal
 }
