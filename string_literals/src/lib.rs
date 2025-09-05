0930b4efeba066cf54839c8ec977a91720d3e2f2
Azalea-u <salmi.mouad137@gmail.com>
2025-09-05 10:05:09 +0100

    ...

--- string_literals/src/lib.rs
+++ string_literals/src/lib.rs
@@ -6,10 +6,14 @@ pub fn is_ascii(v: &str) -> bool {
     v.is_ascii()
 }
 
-pub fn split_at(v: &str, index: usize) -> (&str, &str) {
-    v.split_at(index)
+pub fn contains(s: &str, pat: &str) -> bool {
+    s.contains(pat)
 }
 
-pub fn find(v: &str, pat: char) -> usize {
-    v.find(pat).unwrap_or(0)
+pub fn split_at(s: &str, idx: usize) -> (&str, &str) {
+    s.split_at(idx)
+}
+
+pub fn find(s: &str, pat: char) -> usize {
+    s.find(pat).unwrap()
 }
