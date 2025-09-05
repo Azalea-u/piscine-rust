9bd86591a9a01ed1d293cff43ba83457b1df8e42
Azalea-u <salmi.mouad137@gmail.com>
2025-09-05 10:12:25 +0100

    ...

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
     let mut initials = Vec::new();
     for name in names {
-        initials.push(name.chars().collect::<Vec<char>>()[0].to_string());
+        initials.push(name.chars().collect::<Vec<char>>()[0].to_string() + ".");
     }
     initials
 }
