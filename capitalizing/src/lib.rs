1b10d9e447fbded9d8a8fac64debf18ca60a0fd9
Azalea-u <salmi.mouad137@gmail.com>
2025-09-07 10:09:58 +0100

    ...

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -7,11 +7,20 @@ pub fn capitalize_first(input: &str) -> String {
 }
 
 pub fn title_case(input: &str) -> String {
-    let mut c = input.chars();
-    match c.next() {
-        None => String::new(),
-        Some(first) => first.to_uppercase().to_string() + c.as_str(),
+    let mut should_capitalize = true;
+    let mut result = String::new();
+    for c in input.chars() {
+        if c.is_whitespace() {
+            should_capitalize = true;
+        } else if should_capitalize {
+            result.push(c.to_uppercase().next().unwrap());
+            should_capitalize = false;
+            continue
+        }
+        result.push(c);
+        
     }
+    return result
 }
 
 pub fn change_case(input: &str) -> String {
