11588c8ad75d60bb42cdb3b62b1134d6112efde2
Azalea-u <salmi.mouad137@gmail.com>
2025-09-11 12:22:12 +0100

    ...

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -11,11 +11,11 @@ pub struct Three {
 }
 
 pub struct Four {
-    pub fourth_layer: Option<i32>,
+    pub fourth_layer: Option<u16>,
 }
 
 impl One {
-    pub fn get_fourth_layer(self) -> Option<i32> {
+    pub fn get_fourth_layer(self) -> Option<u16> {
         self.first_layer?.second_layer?.third_layer?.fourth_layer
     }
 }
