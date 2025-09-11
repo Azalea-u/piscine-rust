a0d2166fe4e3d18edbb6e0508bfc28efaa86235b
Azalea-u <salmi.mouad137@gmail.com>
2025-09-11 11:58:26 +0100

    ...

---
+++ handling/src/lib.rs
@@ -0,0 +1,13 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
+
+pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+    let mut file = match File::options().append(true).create(true).open(path) {
+        Ok(file) => file,
+        Err(error) => panic!("Failed to open or create file: {}", error),
+    };
+    if let Err(error) = file.write_all(content.as_bytes()) {
+        panic!("Failed to write to file: {}", error);
+    }
+}
