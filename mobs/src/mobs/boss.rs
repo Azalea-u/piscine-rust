4babdff2825ca9b27d474e8706b14d5f7462e037
Azalea-u <salmi.mouad137@gmail.com>
2025-09-14 09:11:57 +0100

    ...

--- mobs/src/mobs/boss.rs
+++ mobs/src/mobs/boss.rs
@@ -1,11 +1,13 @@
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Boss {
     pub name: String,
-    pub age: u8,
+    pub age: u32,
 }
 
 impl Boss {
-    pub fn new(name: &str, age: u8) -> Boss {
-        Boss { name:name.to_string(), age }
+    pub fn new(name: &str, age: u32) -> Boss {
+        Boss {
+            name: name.to_string(), age
+        }
     }
 }
