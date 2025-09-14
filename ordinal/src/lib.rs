724f69cbdfa60087241d58a55bb43d7cf13dc530
Azalea-u <salmi.mouad137@gmail.com>
2025-09-14 10:01:34 +0100

    ...

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    match x%10 {
+        1 => format!("{}st", x),
+        2 => format!("{}nd", x),
+        3 => format!("{}rd", x),
+        _ => format!("{}th", x),
+    }
+}
