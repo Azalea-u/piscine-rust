a5b6785fd0507def23f25cd8bdcd4010f6baa72d
Azalea-u <salmi.mouad137@gmail.com>
2025-09-14 11:08:32 +0100

    ...

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,3 +1,14 @@
-pub fn  logic_number(num: i32) -> bool {
-    num % 3 == 0 || num % 5 == 0
+pub fn  number_logic(num: i32) -> bool {
+    let mut tmp = num;
+    let mut sum = 0;
+    let mut degits = Vec::new();
+
+    while tmp > 0 {
+        degits.push(tmp % 10);
+        tmp /= 10;
+    }
+    for i in &degits {
+        sum += i.pow(degits.len() as u32);
+    }
+    sum == num
 }
