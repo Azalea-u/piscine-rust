79d5d04ba5be1e88c6f9492644644a3342b89f5f
Azalea-u <salmi.mouad137@gmail.com>
2025-09-19 09:51:58 +0100

    ...

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,21 @@
+use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Self {
+        Self { ref_list }
+    }
+    pub fn add_element(&mut self, elem: Rc<String>) {
+        self.ref_list.push(elem);
+    }
+    pub fn rm_all_ref(&mut self, elem: Rc<String>) {
+        self.ref_list.retain(|x| x != &elem);
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    Rc::strong_count(ref_list)
+}
